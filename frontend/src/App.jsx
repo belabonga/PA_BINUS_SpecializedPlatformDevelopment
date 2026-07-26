7fb939ad5636676a2f9617bd87d814a768628be4
Shania <shania1faza@gmail.com>
2026-07-27 05:00:50 +0700

    Add Google Analytics Tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Toast from './components/Toast.jsx';
-import Home from './pages/Home.jsx';
-import ProductList from './pages/ProductList.jsx';
-import ProductDetail from './pages/ProductDetail.jsx';
-import Wishlist from './pages/Wishlist.jsx';
-import Cart from './pages/Cart.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import Payment from './pages/Payment.jsx';
-import { getToken } from './utils/auth.js';
+import React from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Toast from "./components/Toast.jsx";
+import Home from "./pages/Home.jsx";
+import ProductList from "./pages/ProductList.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+import Wishlist from "./pages/Wishlist.jsx";
+import Cart from "./pages/Cart.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Payment from "./pages/Payment.jsx";
+import { getToken } from "./utils/auth.js";
+import { useLocation } from "react-router-dom";
+import { trackPageView } from "./utils/analytics.js";
 
 function ProtectedRoute({ children }) {
   const location = useLocation();
@@ -24,6 +26,12 @@ function ProtectedRoute({ children }) {
 }
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location.pathname, location.search]);
+
   return (
     <>
       <Navbar />
